55f5432b08cc41d1eeec722c8d1760932b04e8cb
Will Starling (Macbook Air) <willstar2003@gmail.com>
2024-02-04 00:02:25 -0500

    Fixed file misspelling and added footer spacing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <footer>
       <div className='footer-socials'>
-        <a href='https://www.linkedin.com/in/willstarling/' rel='noreferrer' target='_blank'><img src='LinkedIn-button.png' alt='LinkedIn Logo' /></a>
-        <a href='https://github.com/willisaur' rel='noreferrer' target='_blank'><img src='GitHub-button-white.png' alt='GitHub Logo' /></a>
+        <a href='https://www.linkedin.com/in/willstarling/' rel='noreferrer' target='_blank'><img src='./images/LinkedIn Logo.png' alt='LinkedIn Logo' /></a>
+        <a href='https://github.com/willisaur' rel='noreferrer' target='_blank'><img src='./images/GitHub Logo.png' alt='GitHub Logo' /></a>
       </div>
       <h6>Will Starling &copy; 2024</h6>
     </footer>
